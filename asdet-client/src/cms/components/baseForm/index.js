5112fed7b39faa50f2c9f2ce0f0b7dbf95b89c73
sjhty <sun_j1986@163.com>
2019-04-24 22:54:49 +0800

    master

--- asdet-client/src/cms/components/baseForm/index.js
+++ asdet-client/src/cms/components/baseForm/index.js
@@ -19,7 +19,7 @@ class BaseForm extends Component {
                 let width = item.width;
 
                 if (type == 'INPUT') {
-                    const INPUT = <FormItem label={label} key={field}>
+                    const INPUT = <FormItem label={label}>
                         {
                             getFieldDecorator([field])(
                                 <Input placeholder={placeholder} style={{width:width}}/>                        
@@ -28,7 +28,7 @@ class BaseForm extends Component {
                     </FormItem>
                     formItemList.push(INPUT);
                 } else if (type == 'SELECT') {
-                    const SELECT = <FormItem label={label} key={field}>
+                    const SELECT = <FormItem label={label}>
                         {
                             getFieldDecorator([field],{
                                 initialValue: initialValue
@@ -43,14 +43,14 @@ class BaseForm extends Component {
                     </FormItem>
                     formItemList.push(SELECT);
                 } else if (type == 'TIME_SELECT') {
-                    const BEGIN_TIME = <FormItem label={label} key={field}>
+                    const BEGIN_TIME = <FormItem label={label}>
                         {
                             getFieldDecorator('begin_time')(
                                 <DatePicker placeholder='开始时间' showTime={true} format='YYYY-MM-DD HH:mm:ss'/>
                             )
                         }
                     </FormItem>
-                    const END_TIME = <FormItem label={label} key={field}>
+                    const END_TIME = <FormItem label='~' colon={false} >
                         {
                             getFieldDecorator('end_time')(
                                 <DatePicker placeholder='结束时间' showTime={true} format='YYYY-MM-DD HH:mm:ss'/>
